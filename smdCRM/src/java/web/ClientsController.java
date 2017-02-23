0b8fe66fdb9fc69f47392a26dec7014d1f7bed12
sethdumas <sethdumas@gmail.com>
2017-02-22 19:21:23 -0600

    Merge origin/master into sethdumas-006
    Conflicts:
    	ACA2016/resources/config/sethdumas/ChinookManager.properties

--- smdCRM/src/java/web/ClientsController.java
+++ smdCRM/src/java/web/ClientsController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package web;
 
 /**
@@ -19,9 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import objects.Clients;
-import repository.ClientsDAO;
 import validation.ClientsValidator;
-import objects.Message;        
+import objects.Message;
 import java.util.HashMap;
 import java.util.logging.Logger;
 import repository.ClientsDAO;
@@ -31,23 +29,22 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
-
 @Controller
-public class ClientsController{
-    
-     private static final Logger logger = Logger.getLogger(ClientsController.class.getName());
-    
-     @Autowired
+public class ClientsController {
+
+    private static final Logger logger = Logger.getLogger(ClientsController.class.getName());
+
+    @Autowired
     ClientsDAO dao;
-    
+
     @Autowired
     private ClientsValidator clientsValidator;
-    
+
     @RequestMapping("/clients/clientsform")
-    public ModelAndView showform(){
-        return new ModelAndView("clientsform","clients",new Clients());
+    public ModelAndView showform() {
+        return new ModelAndView("clientsform", "clients", new Clients());
     }
-    
+
     @RequestMapping(value = "/clients/save", method = RequestMethod.POST)
     public ModelAndView save(@ModelAttribute("clients") @Valid Clients clients, BindingResult result, HttpServletRequest request) {
         if (result.hasErrors()) {
@@ -61,101 +58,99 @@ public class ClientsController{
         } else {
             msg = new Message(Message.Level.ERROR, "New client creation failed");
         }
-        
+
         request.getSession().setAttribute("message", msg);
-        
+
         return new ModelAndView("redirect:/clients/viewclients");
     }
-    
+
     @RequestMapping("/client/viewclients")
     public ModelAndView viewclients(HttpServletRequest request) {
         return this.viewclients(1, request);
     }
-    
+
     @RequestMapping("/clients/viewclients/{pageid}")
-    public ModelAndView viewclients(@PathVariable int pageid, HttpServletRequest request){
+    public ModelAndView viewclients(@PathVariable int pageid, HttpServletRequest request) {
         int total = 25;
         int start = 1;
-        
-        if(pageid != 1) {
-            start = (pageid-1) * total + 1;  
-        }  
-        
+
+        if (pageid != 1) {
+            start = (pageid - 1) * total + 1;
+        }
+
         List<Clients> list = dao.getClientsByPage(start, total);
-        
+
         HashMap<String, Object> context = new HashMap<String, Object>();
         context.put("list", list);
-        
+
         int count = dao.getClientsCount();
-        context.put("pages", Math.ceil((float)count/(float)total));
-        
+        context.put("pages", Math.ceil((float) count / (float) total));
+
         context.put("page", pageid);
-        
-        Message msg = (Message)request.getSession().getAttribute("message");
+
+        Message msg = (Message) request.getSession().getAttribute("message");
 
         if (msg != null) {
             context.put("message", msg);
             request.getSession().removeAttribute("message");
         }
-       
+
         return new ModelAndView("viewclients", context);
     }
-    
+
     @RequestMapping(value = "/clients/editclients/{id}")
-    public ModelAndView edit(@PathVariable int id){
+    public ModelAndView edit(@PathVariable int id) {
         Clients clients = dao.getClientById(id);
-        return new ModelAndView("clientseditform","clients",clients);
+        return new ModelAndView("clientseditform", "clients", clients);
     }
-    
-    @RequestMapping(value = "/clients/editsave",method = RequestMethod.POST)
-    public ModelAndView editsave(@ModelAttribute("clients") @Valid Clients clients, BindingResult result, HttpServletRequest request){
-        if(result.hasErrors()){
+
+    @RequestMapping(value = "/clients/editsave", method = RequestMethod.POST)
+    public ModelAndView editsave(@ModelAttribute("clients") @Valid Clients clients, BindingResult result, HttpServletRequest request) {
+        if (result.hasErrors()) {
             return new ModelAndView("clientseditform", "clients", clients);
         }
-        
+
         int r = dao.update(clients);
-        
+
         Message msg = null;
         if (r == 1) {
             msg = new Message(Message.Level.INFO, "Client has been successfully saved");
-        }
-        else {
+        } else {
             msg = new Message(Message.Level.ERROR, "Edit client failed");
         }
-        
+
         request.getSession().setAttribute("message", msg);
-        
+
         return new ModelAndView("redirect:/clients/viewclients");
     }
-    
-    @RequestMapping(value = "/clients/deleteclient/{id}",method = RequestMethod.GET)
-    public ModelAndView delete(@PathVariable int id, HttpServletRequest request){
+
+    @RequestMapping(value = "/clients/deleteclient/{id}", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable int id, HttpServletRequest request) {
         int r = dao.delete(id);
-        
+
         Message msg = null;
         if (r == 1) {
             msg = new Message(Message.Level.INFO, "Client has been successfully deleted");
-        }
-        else {
+        } else {
             msg = new Message(Message.Level.ERROR, "Delete client failed");
         }
-        
+
         request.getSession().setAttribute("message", msg);
-        
+
         return new ModelAndView("redirect:/clients/viewclients");
     }
-    
+
     @InitBinder("clients")
-    public void initBinder(WebDataBinder webDataBinder){
+    public void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.setValidator(clientsValidator);
     }
-    
+
     public ClientsValidator getClientsValidator() {
         return clientsValidator;
     }
- 
+
     public void setClientsValidator(ClientsValidator clientsValidator) {
         this.clientsValidator = clientsValidator;
-    }  
+    }
 
 }
